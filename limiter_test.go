f5f7222e4d9ca48ca8c76e9c8bb9d8f2c253d61c
Zhibin Dong <developerdong@gmail.com>
2021-12-31 11:22:03 +0800

    refactor: test case

--- limiter_test.go
+++ limiter_test.go
@@ -10,7 +10,11 @@ func TestLimiter(t *testing.T) {
 	n := 60
 	duration := 10 * time.Second
 	limiter := New(int64(n), duration)
+	startTime := time.Now()
 	for i := 0; i < n+1; i++ {
 		_ = limiter.Wait(context.Background())
+	}
+	if time.Now().Sub(startTime) <= duration {
+		t.Error("the number of requests exceeds the limit")
 	}
 }
